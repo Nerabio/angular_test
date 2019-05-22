6b0f1f06aa484a379d8827450eb0f13529e8584f
Alexander <Nerey@bk.ru>
2019-05-23 00:12:02 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryService } from './country.service';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllCountriesComponent,
+    CountryDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CountryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
